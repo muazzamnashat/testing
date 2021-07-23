b919fce395b6767423f6da277967e225087965ae
Muazzam Nashat <mnashat@shiftdigital.com>
2021-07-23 03:40:54 -0400

    testing

--- apps/movies/src/app/app.component.ts
+++ apps/movies/src/app/app.component.ts
@@ -14,5 +14,6 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     // this.movies = (data as any).default;
     // this.movies$.subscribe((data) => console.log(data));
+    // for testing
   }
 }
